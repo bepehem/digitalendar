49587ec3cb31ae1a0fb03831d985e81f78379669
bepehem <bpm.tardiveau@gmail.com>
2019-07-04 17:21:35 +0200

    include change

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,26 +4,20 @@ namespace App\Controller;
 
 use App\Entity\Event;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="homepage", methods={"GET"})
      */
-    public function index()
+    public function show() : Response
     {
+        $events =$this->getDoctrine()->getRepository(Event::class)->findAll();
+
         return $this->render('default/homepage.html.twig', [
-            'controller_name' => 'DefaultController',
-        ]);
-    }
-
-    public function Event ()
-    {
-        $event = $this->getDoctrine()->getRepository(Event::class)->findAll();
-
-        return $this->render("default/_event.html.twig", [
-            "event" => $event
+            'events'=>$events
         ]);
     }
 }
